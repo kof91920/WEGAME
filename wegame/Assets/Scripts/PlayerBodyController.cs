dd79e7131b29b04ddd84ff476429984737ec66aa
luxebo <myyuen@uci.edu>
2018-10-16 20:34:34 -0700

    add files 2

---
+++ wegame/Assets/Scripts/PlayerBodyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBodyController : MonoBehaviour
+{
+
+    // is the player taking damage
+    bool isTakingDamage = true;
+
+    // player rpg controller
+    PlayerRpgController playerCtrl;
+
+    void Awake()
+    {
+        playerCtrl = FindObjectOfType<PlayerRpgController>();
+
+        if (playerCtrl == null)
+        {
+            Debug.LogError("The scene needs a PlayerRpgController object");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        // only take damage when the flag is true
+        if (!isTakingDamage) return;
+
+        // check we hit an enemy
+        if (other.CompareTag("Enemy"))
+        {
+            print("enemy attack");
+
+            // get the enemy
+            EnemyController enemy = other.GetComponent<EnemyController>();
+
+            // damage player health
+            playerCtrl.damage(enemy.attack);
+
+            // don't take damage for 1 second
+
+            // set the flag to false
+            isTakingDamage = false;
+
+            Invoke("ActivateTakingDamage", 1);
+        }
+    }
+
+    void ActivateTakingDamage()
+    {
+        isTakingDamage = true;
+    }
+}
---
+++ wegame/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CollectableController))]
+public class WeaponController : MonoBehaviour {
+
+    
+    bool isReady = false;
+
+    CollectableController collectable;
+
+    public void SetReady(bool value)
+    {
+        isReady = value;
+    }
+
+    void Awake()
+    {
+        collectable = GetComponent<CollectableController>();
+    }
+    
+    void OnTriggerStay(Collider other)
+    {
+        // weapon is being swinged
+        if (!isReady) return;
+
+        // check we hit an enemy
+        if(other.CompareTag("Enemy"))
+        {
+            // get the enemy
+            EnemyController enemy = other.GetComponent<EnemyController>();
+
+            // damage the enemy
+            enemy.damage(collectable.GetProperty("attack"));
+
+            // only hit once per swing
+            isReady = false;
+        }
+    }
+}
